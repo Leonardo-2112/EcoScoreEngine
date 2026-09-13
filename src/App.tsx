07190c5842c9b12e783df08743cecc83cb40ccf3
Matheus-Jones <matheus.jones@icloud.com>
2026-09-12 22:56:25 -0300

    feat: adiciona rota dinâmica /integrantes/:i

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,25 @@ import { Sobre } from "./pages/Sobre";
 import { AppSimulador } from "./pages/AppSimulador";
 import { Empresas } from "./pages/Empresas";
 import { Integrantes } from "./pages/Integrantes";
+import { IntegranteDetalhe } from "./pages/IntegranteDetalhe";
 import { Faq } from "./pages/Faq";
 import { Contato } from "./pages/Contato";
  
  
 function App() {
   return (
-<Routes>
-<Route path="/" element={<Layout />}>
-<Route index element={<Home />} />
-<Route path="sobre" element={<Sobre />} />
-<Route path="app" element={<AppSimulador />} />
-<Route path="empresas" element={<Empresas />} />
-<Route path="integrantes" element={<Integrantes />} />
-<Route path="faq" element={<Faq />} />
-<Route path="contato" element={<Contato />} />
-</Route>
-</Routes>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="sobre" element={<Sobre />} />
+            <Route path="app" element={<AppSimulador />} />
+            <Route path="empresas" element={<Empresas />} />
+            <Route path="integrantes" element={<Integrantes />} />
+            <Route path="integrantes/:id" element={<IntegranteDetalhe />} />
+            <Route path="faq" element={<Faq />} />
+            <Route path="contato" element={<Contato />} />
+        </Route>
+    </Routes>
   );
 }
  
