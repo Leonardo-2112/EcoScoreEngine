f6c93d464df026eb87ade90385a1c55a02a644b4
Matheus-Jones <matheus.jones@icloud.com>
2026-09-12 22:52:17 -0300

    feat: configura rotas estáticas das páginas principais

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Sobre } from "./pages/Sobre";
+import { AppSimulador } from "./pages/AppSimulador";
+import { Empresas } from "./pages/Empresas";
+import { Integrantes } from "./pages/Integrantes";
+import { Faq } from "./pages/Faq";
+import { Contato } from "./pages/Contato";
+ 
+ 
+function App() {
+  return (
+<Routes>
+<Route path="/" element={<Layout />}>
+<Route index element={<Home />} />
+<Route path="sobre" element={<Sobre />} />
+<Route path="app" element={<AppSimulador />} />
+<Route path="empresas" element={<Empresas />} />
+<Route path="integrantes" element={<Integrantes />} />
+<Route path="faq" element={<Faq />} />
+<Route path="contato" element={<Contato />} />
+</Route>
+</Routes>
+  );
+}
+ 
+export default App;
